bbf1adbf624e1def11aef96695b8cbe5e11bc2aa
storub <haldon31@mail.ru>
2020-12-21 21:05:40 +0300

    add getopt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
+#include "unistd.h"
+#include "getopt.h"
 
-int main()
+int port_opt;
+char *ip_opt;
+char *dir_opt;
+
+
+extern char *optarg;
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "start" << std::endl;
+
+    int opindex;
+    char opchar;
+    struct option opts[] = {
+            {"ip",   required_argument, 0, 0},
+            {"port", required_argument, 0, 0},
+            {"dir",  required_argument, 0, 0},
+            {0, 0,                      0, 0,}
+    };
+
+
+    while((opchar = getopt_long(argc, argv, "h:p:d:", opts, &opindex)) != -1 )
+    {
+        switch( opchar )
+        {
+            case 'h' :
+                ip_opt = optarg;
+                break;
+            case 'p' :
+                port_opt = atoi(optarg);
+                break;
+            case 'd' :
+                dir_opt = optarg;
+                break;
+            case 0:
+                break;
+            case '?':
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+    std::cout << "ip=" << ip_opt << std::endl;
+    std::cout << "port=" << port_opt << std::endl;
+    std::cout << "dir=" << dir_opt << std::endl;
+
     return 0;
 }
